01703d468b7b27e1738caf28ca8699109d130702
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-03-08 22:08:32 -0300

    Deu ruim

--- src/model/ModelAluno.java
+++ src/model/ModelAluno.java
@@ -27,13 +27,6 @@ public class ModelAluno {
     }
 
     public void setNomeAluno(String nomeAluno) {
-        if (this.getNomeAluno() == null) {
-            String vazio = "Nome do aluno em branco!!";
-            throw new IllegalArgumentException(vazio);
-        }
-        if (this.getNomeAluno().length() > 50) {
-            throw new IllegalArgumentException("O nome do aluno deve ter até 50 caracteres!!");
-        }
         this.nomeAluno = nomeAluno;
     }
     
